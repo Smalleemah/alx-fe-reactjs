ae61836c644826a7e07b59a0ae888bdcb409ca43
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-02-07 13:54:55 +0100

    Updated Commit

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,11 +1,7 @@
 import UserProfile from "./components/UserProfile";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center px-4">
-      <UserProfile />
-    </div>
-  );
+  return <UserProfile />;
 }
 
 export default App;
--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,39 +1,49 @@
 function UserProfile() {
   return (
     <div
-      className="bg-gray-100 
-                    p-4 sm:p-6 md:p-8 
-                    max-w-xs sm:max-w-xs md:max-w-sm 
-                    mx-auto my-8 md:my-16 
-                    rounded-lg shadow-lg"
+      className="
+        bg-gray-100
+        p-4 md:p-8
+        max-w-xs md:max-w-sm
+        mx-auto my-10 md:my-20
+        rounded-lg shadow-lg
+        text-center
+      "
     >
-      <div className="flex flex-col items-center text-center">
-        {/* Profile Image */}
-        <img
-          src="https://via.placeholder.com/150"
-          alt="User"
-          className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 
-                     rounded-full mb-4"
-        />
+      {/* Profile Image */}
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="
+          w-24 h-24
+          md:w-36 md:h-36
+          rounded-full
+          mx-auto
+        "
+      />
 
-        {/* Name */}
-        <h1
-          className="text-lg sm:text-lg md:text-xl 
-                       text-blue-800 font-semibold mb-2"
-        >
-          John Doe
-        </h1>
+      {/* User Name */}
+      <h1
+        className="
+          text-lg md:text-xl
+          text-blue-800
+          font-semibold
+          my-4
+        "
+      >
+        John Doe
+      </h1>
 
-        {/* Bio */}
-        <p
-          className="text-gray-600 
-                      text-sm sm:text-sm md:text-base 
-                      leading-relaxed"
-        >
-          Developer at Example Co. Loves to write code and explore new
-          technologies.
-        </p>
-      </div>
+      {/* User Bio */}
+      <p
+        className="
+          text-gray-600
+          text-sm md:text-base
+        "
+      >
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
+      </p>
     </div>
   );
 }
