d277d6b151c76731fba4b9728796ca56d5ac0e87
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-02-07 13:59:43 +0100

    Updated Changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ function UserProfile() {
     <div
       className="
         bg-gray-100
-        p-4 md:p-8
-        max-w-xs md:max-w-sm
+        sm:p-4 md:p-8
+        max-w-xs sm:max-w-xs md:max-w-sm
         mx-auto my-10 md:my-20
         rounded-lg shadow-lg
         text-center
@@ -15,20 +15,22 @@ function UserProfile() {
         src="https://via.placeholder.com/150"
         alt="User"
         className="
-          w-24 h-24
+          sm:w-24 sm:h-24
           md:w-36 md:h-36
           rounded-full
           mx-auto
+          mb-4
         "
       />
 
       {/* User Name */}
       <h1
         className="
-          text-lg md:text-xl
+          sm:text-lg
+          md:text-xl
           text-blue-800
           font-semibold
-          my-4
+          mb-2
         "
       >
         John Doe
@@ -37,8 +39,10 @@ function UserProfile() {
       {/* User Bio */}
       <p
         className="
+          sm:text-sm
+          md:text-base
           text-gray-600
-          text-sm md:text-base
+          leading-relaxed
         "
       >
         Developer at Example Co. Loves to write code and explore new
