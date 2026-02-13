b76e82c3dd6e649ccc0a70ca0919993c8482a29c
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-02-14 00:03:12 +0100

    commit changes

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -9,18 +9,16 @@ export default function HomePage() {
     setRecipes(recipesData);
   }, []);
 
-  if (recipes.length === 0) {
-    return <h1 className="text-center mt-10">Loading recipes...</h1>;
-  }
-
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center">Recipes</h1>
+      <h1 className="text-3xl font-bold text-center mb-6">
+        Recipe Sharing Platform
+      </h1>
 
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
-            <div className="bg-white rounded-lg shadow-md hover:shadow-xl hover:scale-105 transition duration-300">
+            <div className="bg-white rounded-lg shadow-md hover:shadow-xl hover:scale-105 transition duration-300 cursor-pointer">
               <img
                 src={recipe.image}
                 alt={recipe.title}
