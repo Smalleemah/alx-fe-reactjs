3fab817a3af419c1c44b6433e363165c9323b35f
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-02-21 10:25:15 +0100

    commit change

---
+++ form-handling-react/src/components/FormikForm.jsx
@@ -0,0 +1,67 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const FormikForm = () => {
+  // Validation Schema
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  });
+
+  return (
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
+        console.log("Formik Data:", values);
+
+        // Mock API
+        fetch("https://jsonplaceholder.typicode.com/users", {
+          method: "POST",
+          body: JSON.stringify(values),
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("Response:", data);
+            alert("Formik Registration Successful!");
+            resetForm();
+          });
+      }}
+    >
+      <Form>
+        <h2>Formik Registration Form</h2>
+
+        <div>
+          <Field name="username" placeholder="Username" />
+          <ErrorMessage name="username" component="p" />
+        </div>
+
+        <div>
+          <Field name="email" type="email" placeholder="Email" />
+          <ErrorMessage name="email" component="p" />
+        </div>
+
+        <div>
+          <Field name="password" type="password" placeholder="Password" />
+          <ErrorMessage name="password" component="p" />
+        </div>
+
+        <button type="submit">Register</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default FormikForm;
