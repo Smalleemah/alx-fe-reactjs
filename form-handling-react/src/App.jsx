9ff63b586602570f1f48b8a8f60a50c8c4ef77d6
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-02-21 11:06:41 +0100

    update change

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import PostsComponent from "./components/PostsComponent";
 
+// Create query client
 const queryClient = new QueryClient();
 
 function App() {
-  // Toggle to demonstrate caching
-  const [showPosts, setShowPosts] = useState(true);
-
   return (
     <QueryClientProvider client={queryClient}>
-      <button onClick={() => setShowPosts(!showPosts)}>Toggle Posts</button>
-
-      {showPosts && <PostsComponent />}
+      <PostsComponent />
     </QueryClientProvider>
   );
 }
