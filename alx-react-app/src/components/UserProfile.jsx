1882c41976cbc58d4bb107ba8a9989924ac47165
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-03-01 00:04:25 +0100

    commit

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-
-function UserProfile(props) {
+const UserProfile = (props) => {
   return (
     <div>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
-      <p>{props.bio}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
-}
+};
 
 export default UserProfile;
