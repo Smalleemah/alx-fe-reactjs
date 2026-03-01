afe1172cc1f1672ec3ef1f7c0920cd5dd1466340
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-03-01 21:27:34 +0100

    commit change

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,7 +1,16 @@
-import UserDetails from "./UserDetails";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
-function UserInfo() {
-  return <UserDetails />;
+function UserDetails() {
+  // Use the useContext hook to access the data
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default UserInfo;
+export default UserDetails;
