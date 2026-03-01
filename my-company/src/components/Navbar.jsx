bee3bfa13173e6204face35ad4ba47119003bf9c
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-03-01 10:10:07 +0100

    update

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,17 +2,25 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "navy" }}>
-      <Link to="/" style={{ margin: "0 10px", color: "white" }}>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        padding: "10px",
+        backgroundColor: "navy",
+      }}
+    >
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
         Home
       </Link>
-      <Link to="/about" style={{ margin: "0 10px", color: "white" }}>
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
         About
       </Link>
-      <Link to="/services" style={{ margin: "0 10px", color: "white" }}>
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>
         Services
       </Link>
-      <Link to="/contact" style={{ margin: "0 10px", color: "white" }}>
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>
         Contact
       </Link>
     </nav>
