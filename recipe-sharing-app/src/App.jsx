1bc77db529abebbcc9698bd3e81e74c7f0be4eb0
Small_leemah22 <alimuslimahopeyemi@gmail.com>
2026-03-01 10:58:24 +0100

    commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,23 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetail from "./components/RecipeDetail";
+
 function App() {
-  return <Router>{/* your components */}</Router>;
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
